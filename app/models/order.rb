73f6678d05e9eaa9fdda20c255bd7cb3c6056754
y1220 <ga.2.y1220@gmail.com>
2022-04-24 11:57:01 +0200

    (model): refine references of optional product

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :product_package
-  belongs_to :optional_product
   belongs_to :customer
   belongs_to :validity_period
+  has_many :optional_product_orders
+  has_many :optional_products, through: :optional_product_orders
 end
