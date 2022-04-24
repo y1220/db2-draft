69a83f84e8498d6c7e12bb21aed7081753deeb42
y1220 <ga.2.y1220@gmail.com>
2022-04-24 19:04:29 +0200

    (fail payment): add a list in home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,15 @@ class HomeController < ApplicationController
   before_action :set_current_user
 
   def index
+    @customer = Customer.find(session[:customer_id])
+    if @customer
+      if @customer.is_insolvent
+        @is_insolvent = true
+        @orders= Order.where(status: false, customer_id: @customer.id)
+      end
+    else
+      @is_insolvent = false
+      flash[:notice]= "Welcome!"
+    end
   end
 end
